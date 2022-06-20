8a6a10eed6ac5ea723cc2a7e1b7c24ec00eae2e9
Nguyen-Van-Chuong <107835819+Nguyen-Van-Chuong@users.noreply.github.com>
2022-06-20 14:06:35 +0700

    assignment

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    loop: true,
+});
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
+
+$(document).ready(function() {
+    $(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+});
---
+++ js/js.js
@@ -0,0 +1,58 @@
+
+
+// let preveiwContainer = document.querySelector('.products-preview');
+// let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+// document.querySelectorAll('.products-container .product').forEach(product => {
+//   product.onclick = () => {
+//     preveiwContainer.style.display = 'flex';
+//     let name = product.getAttribute('data-name');
+//     previewBox.forEach(preview => {
+//       let target = preview.getAttribute('data-target');
+//       if (name == target) {
+//         preview.classList.add('active');
+//       }
+//     });
+//   };
+// });
+
+// previewBox.forEach(close => {
+//   close.querySelector('.fa-times').onclick = () => {
+//     close.classList.remove('active');
+//     preveiwContainer.style.display = 'none';
+//   };
+// });
+
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 800, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 2000);
+}
+
+window.onload = fadeOut;
